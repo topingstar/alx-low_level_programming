f40c6633317c22b6fd4c7e56900b503b77ce9b96
topister onyango <topisternandera@gmail.com>
2021-06-17 01:55:42 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ *main - funtion display "Programming is like building a multilingual puzzle.
+ *Return: 0
  */
 
 int main(void)
