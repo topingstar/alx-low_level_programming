28eeff2b2f6776d05b6b4df3335ef9575bd0bd46
topister onyango <topisternandera@gmail.com>
2021-06-24 11:00:57 -0700

    print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,21 +1,35 @@
 #include "holberton.h"
 
 /**
- * main - check the code for ALX School students.
- *
- * Return: Always 0.
+ * print_number - Prints an integer
+ * @n: Integer to print
  */
-int main(void)
+
+void print_number(int n)
 {
-print_number(98);
-_putchar('\n');
-print_number(402);
-_putchar('\n');
-print_number(1024);
-_putchar('\n');
-print_number(0);
-_putchar('\n');
-print_number(-98);
-_putchar('\n');
-return (0);
+int copy, nth, size = 1, ones = n % 10;
+
+n /= 10;
+copy = n;
+if (ones < 0)
+{
+ones *= -1, copy *= -1, n *= -1;
+_putchar('-');
 }
+if (copy > 0)
+{
+while (copy / 10 != 0)
+{
+copy /= 10, size *= 10;
+}
+while (size > 0)
+{
+nth = n / size;
+_putchar('0' + nth);
+n -= nth *size;
+size /= 10;
+}
+}
+_putchar('0' + ones);
+}
+
