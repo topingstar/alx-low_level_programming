fcbcebb87feb5d206abaa250086f7d02435702cd
topister onyango <topisternandera@gmail.com>
2021-07-07 12:56:46 -0700

    wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,27 +1,21 @@
 #include "holberton.h"
 
 /**
- * wildcmp - compares two strings
- * @s1: 1st string to be compared
- * @s2: second string containing '*' or not
- * Return: 1 if the strings can be considered as identical, and 0
- **/
+ * wildcmp - compare two strings and returns 1 if identical
+ * @s1: first string
+ * @s2: second string
+ * Return: 1 if identical otherwise 0
+ */
+
 int wildcmp(char *s1, char *s2)
 {
-if (*s1 == '\0' && *s2 == '\0')
+int j = 0;
+
+if (*(s1 + j) == '\0' && *(s2 + j) == '\0')
 return (1);
-if (*s1 == *s2)
-return (wildcmp(s1 + 1, s2 + 1));
-if (*s2 == '\0')
-return (0);
-if (*s2 == '*')
-return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
-if (*s1 == '\0')
-{
-if (*s2 != '*')
-return (0);
-else
-return (wildcmp(s1, s2 + 1));
-}
+if (*(s1 + j) == *(s2 + j))
+return (1 && wildcmp(s1 + 1, s2 + 1));
+if (*(s2 + j) == '*')
+return (wildcmp(s1, s2 + 1) || (*(s1 + j) != '\0' && wildcmp(s1 + 1, s2)));
 return (0);
 }
