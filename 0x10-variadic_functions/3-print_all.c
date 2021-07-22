f9b61805987c6282d7fb52ab413e77f3a3691ca4
topister onyango <topisternandera@gmail.com>
2021-07-22 12:20:42 -0700

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,79 +1,114 @@
 #include "variadic_functions.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
- * _printchar - print char type element from va_list
- * @list: va_list passed to function
- */
-void _printchar(va_list list)
-{
-printf("%c", va_arg(list, int));
-}
-
-/**
- * _printstr - print string element from va_list
- * @list: va_list passed to function
- */
-void _printstr(va_list list)
-{
-char *s;
-
-s = va_arg(list, char *);
-if (s == NULL)
-s = "(nil)";
-printf("%s", s);
-}
-
-/**
- * _printfloat - print float type element from va_list
- * @list: va_list passed to function
- */
-void _printfloat(va_list list)
-{
-printf("%f", va_arg(list, double));
-}
-
-/**
- * _printint - print int type element from va_list
- * @list: va_list passed to function
- */
-void _printint(va_list list)
-{
-printf("%d", va_arg(list, int));
-}
-
-/**
- * print_all - print anything passed if char, int, float, or string.
- * @format: string of formats to use and print
+ * print_all - print everything
+ * @format: format
+ *
+ * Return: void function
  */
 void print_all(const char * const format, ...)
 {
-unsigned int i, j;
+char current;
+int i = 0, index;
 va_list args;
-char *sep;
 
-checker storage[] = {
-{ "c", _printchar },
-{ "f", _printfloat },
-{ "s", _printstr },
-{ "i", _printint }
-};
-
-i = 0;
-sep = "";
 va_start(args, format);
-while (format != NULL && format[i / 4] != '\0')
+index = checker(format);
+while ((format != NULL) && (format[i]))
 {
-j = i % 4;
-if (storage[j].type[0] == format[i / 4])
+current = format[i];
+switch (current)
 {
-printf("%s", sep);
-storage[j].f(args);
-sep = ", ";
-}
+case 'c':
+printf("%c", va_arg(args, int));
+comma(i, index);
 i++;
+break;
+case 'i':
+printf("%d", va_arg(args, int));
+comma(i, index);
+i++;
+break;
+case 'f':
+printf("%f", va_arg(args, double));
+comma(i, index);
+i++;
+break;
+case 's':
+printf("%s", nil(va_arg(args, char *)));
+comma(i, index);
+i++;
+break;
+default:
+i++;
+break;
+}
 }
 printf("\n");
 va_end(args);
+}
+
+
+/**
+ * checker - print strings with given separator
+ * @format: format
+ *
+ * Return: int
+ */
+int checker(const char * const format)
+{
+int i = 0, j = 0;
+char str;
+
+while ((format != NULL) && (format[j]))
+{
+str = format[j];
+switch (str)
+{
+case 'c':
+i = j;
+j++;
+break;
+case 'f':
+i = j;
+j++;
+break;
+case 's':
+i = j;
+j++;
+break;
+case 'i':
+i = j;
+j++;
+break;
+default:
+j++;
+break;
+}
+}
+return (i);
+}
+/**
+ * comma - print comma
+ * @i: current index
+ * @j: last valid index
+ *
+ * Return: void
+ */
+void comma(int i, int j)
+{
+if (i != j)
+printf(", ");
+}
+/**
+ * nil - print strings with given separator
+ * @s: check if its NULL
+ *
+ * Return: (nil) if null
+ */
+char *nil(char *s)
+{
+if (s == NULL)
+s = "(nil)";
+return (s);
 }
