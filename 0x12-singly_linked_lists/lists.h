32aad02fd13b2a93eb187de77d9083a258980c15
topister onyango <topisternandera@gmail.com>
2021-07-29 03:58:02 -0700

    header file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,10 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -24,4 +27,6 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+void before_main(void)__attribute__((constructor));
+
 #endif
