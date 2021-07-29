775331afcff1601798e07bf596dd7e9da893c574
topister onyango <topisternandera@gmail.com>
2021-07-29 03:53:59 -0700

    add node end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,43 +1,52 @@
 #include "lists.h"
-#include <string.h>
-#include <stdio.h>
+
 /**
- * add_node_end - add a new node at the end of `list_t` list
- * @head: double pointer to head
- * @str: string to duplicate into new node
- * Return: Address of the new element or NULL if failed
+ * _strlen - Returns the lenght of a string.
+ * @s: Type char pointer
+ * Return: Always 0.
+ */
+int _strlen(const char *s)
+{
+	unsigned int len;
+
+	for (len = 0; s[len] != '\0'; len++)
+	{
+	}
+	return (len);
+
+}
+
+/**
+ * add_node_end - Returns the lenght of a string.
+ * @str: Type char pointer
+ * @head: type pointer of struct
+ * Return: new_node.
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *current;
-	list_t *new_node;
-	int c;
+	list_t *n_node, *tmp_n_d;
 
-	current = *head;
-	while (current && current->next != NULL)
-		current = current->next;
+	n_node = malloc(sizeof(list_t));
 
-	for (c = 0; dup_str[c] != '\0'; c++)
-		;
-
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
-	{
-		free(new_node);
+	if (n_node == NULL)
 		return (NULL);
-	}
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	new_node->len = c;
-	new_node->next = NULL;
 
-	if (current)
-		current->next = new_node;
+	n_node->str = strdup(str);
+	n_node->len = _strlen(str);
+	n_node->next = NULL;
+
+	if (!*head)
+	{
+		*head = n_node;
+	}
 	else
-		*head = new_node;
-	return (new_node);
+	{
+		tmp_n_d = *head;
+		while (tmp_n_d->next)
+			tmp_n_d = tmp_n_d->next;
+		{
+			tmp_n_d->next = n_node;
+		}
+	}
+	return (n_node);
 }
