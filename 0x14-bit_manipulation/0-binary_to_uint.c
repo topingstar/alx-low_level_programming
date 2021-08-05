f6c47eae4321006136be31ddc8c5f73d92087c4e
topister onyango <topisternandera@gmail.com>
2021-08-05 03:43:01 -0700

    binary to uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * binary_to_uint - Converts a binary number to an unsigned int.
@@ -29,3 +29,4 @@ unsigned int binary_to_uint(const char *b)
 
 	return (num);
 }
+
