1b038684ad20fe656bfd46314075f19d912535c1
topister onyango <topisternandera@gmail.com>
2021-08-05 03:31:22 -0700

    header file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,9 +1,7 @@
-#ifndef BIT_MANIPULATION
-#define BIT_MANIPULATION
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include <holberton.h>
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
@@ -13,4 +11,5 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
+
 #endif
