f01ca0cc46ff84eca3fef1f2857d53391795e51f
topister onyango <topisternandera@gmail.com>
2021-08-05 02:27:17 -0700

    header file

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,5 +1,10 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef BIT_MANIPULATION
+#define BIT_MANIPULATION
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <holberton.h>
+
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
