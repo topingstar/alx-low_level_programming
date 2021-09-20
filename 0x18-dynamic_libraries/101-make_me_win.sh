2987758f272475b4d6c0febdcddddbe72bcebecb
topister onyango <topisternandera@gmail.com>
2021-09-20 13:18:10 -0700

    add difficult

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/JoeKAboagye/alx-low_level_programming/blob/8f5e5ffadf6bc45872e1cb9e84ba4538f80f29db/0x18-dynamic_libraries/idontknow.so
-export LD_PRELOAD=/tmp/idontknow.so
+wget -P /tmp https://github.com/topingstar/alx-low_level_programming/blob/644bfa000c7f8fdad41fa08bb2f2f5afd79b66fd/0x18-dynamic_libraries/difficult.so
+export LD_PRELOAD=/tmp/difficult.so
