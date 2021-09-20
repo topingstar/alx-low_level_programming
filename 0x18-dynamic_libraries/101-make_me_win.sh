8b5e28fa1dd1b6e577e629d39635ea961ad3462a
topister onyango <topisternandera@gmail.com>
2021-09-20 12:36:53 -0700

    win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cp ../sh2.sh .
-. ./sh2.sh
+wget -P /tmp https://github.com/JoeKAboagye/alx-low_level_programming/blob/8f5e5ffadf6bc45872e1cb9e84ba4538f80f29db/0x18-dynamic_libraries/idontknow.so
+export LD_PRELOAD=/tmp/idontknow.so
