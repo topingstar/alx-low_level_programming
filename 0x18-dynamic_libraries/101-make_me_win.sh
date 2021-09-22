d665610ee83bd6422a91a59d25944712a1eea842
topister onyango <topisternandera@gmail.com>
2021-09-22 12:25:51 -0700

    make me win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/topingstar/alx-low_level_programming/blob/644bfa000c7f8fdad41fa08bb2f2f5afd79b66fd/0x18-dynamic_libraries/difficult.so
-export LD_PRELOAD=/tmp/difficult.so
+wget -q --output-document=$HOME/difficult.so https://github.com/topingstar/alx-low_level_programming/blob/main/0x18-dynamic_libraries/difficult.so?raw=true
+export LD_PRELOAD=$HOME/difficult.so
