17a89655d15c31dff4ff9e099bc875681732a665
topisteronyango <t.onyango@alustudent.com>
2022-01-19 08:21:30 -0800

    linear skip

---
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -0,0 +1,19 @@
+#include "search_algos.h"
+
+/**
+ * linear_skip - searches for a value in a sorted skip list of integers.
+ * @list: pointer to the head of the skip list to search in.
+ * @value: value to search for.
+ * Return: pointer to the first node where value is located.
+ * resturns NULL if value is not present in the list or if head is NULL.
+ **/
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	if (!list)
+		return (list);
+
+	/* It's not what you expected, I still have to do this task */
+	value = value;
+
+	return (list);
+}
