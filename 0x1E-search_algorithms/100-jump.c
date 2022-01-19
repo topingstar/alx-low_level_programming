5a396fc8f03e534368ea92e5850d9733c8b13b23
topisteronyango <t.onyango@alustudent.com>
2022-01-19 08:39:18 -0800

    jump

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,20 +1,36 @@
 #include "search_algos.h"
 
 /**
- * jump_search - searches for a value in a sorted array of integers,
- * using the Jump search algorithm.
- * @array: pointer to the first element of the array to search in.
- * @size: number of elements in array.
- * @value: value to search for.
- * Return: first index where value is located,
- * or -1 if value is not present or array is NULL.
- **/
+  * jump_search - Searches using jump search.
+  * @array: A pointer to the first element of the array to search.
+  * @size: The size of the array.
+  * @value: The value to search for.
+  *
+  * Return: NULL, -1, the first index where the value is located.
+  */
 int jump_search(int *array, size_t size, int value)
 {
-	if (!array)
+	size_t i, jump, step;
+
+	if (array == NULL || size == 0)
 		return (-1);
 
-	/* It's not what you expected, I still have to do this task */
+	step = sqrt(size);
+	for (i = jump = 0; jump < size && array[jump] < value;)
+	{
+		printf("Value checked array[%ld] = [%d]\n", jump, array[jump]);
+		i = jump;
+		jump += step;
+	}
 
-	return (-1);
+	printf("Value found between indexes [%ld] and [%ld]\n", i, jump);
+
+	if (jump >= size)
+		jump = size - 1;
+
+	for (; i < jump && array[i] < value; i++)
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+	printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+
+	return (array[i] == value ? (int)i : -1);
 }
