421efd15bea499cd415e3a0b024e1b0afe9e7198
topisteronyango <t.onyango@alustudent.com>
2022-01-19 08:19:35 -0800

    jump list

---
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -0,0 +1,22 @@
+#include "search_algos.h"
+
+/**
+ * jump_list - searches for a value in a sorted list of integers
+ * using the Jump search algorithm.
+ * @list: pointer to the head of the list to search in.
+ * @size: number of nodes in the list.
+ * @value: value to search for.
+ * Return: pointer to the first node where value is located.
+ * resturns NULL if value is not present in the list or if head is NULL.
+ **/
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+	if (!list)
+		return (list);
+
+	/* It's not what you expected, I still have to do this task */
+	size = size;
+	value = value;
+
+	return (list);
+}
