abc205310234477e24d3657c2d7de5df70af2f25
topisteronyango <t.onyango@alustudent.com>
2022-01-19 10:21:41 -0800

    linear skip

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -1,19 +1,46 @@
 #include "search_algos.h"
 
 /**
- * linear_skip - searches for a value in a sorted skip list of integers.
- * @list: pointer to the head of the skip list to search in.
- * @value: value to search for.
- * Return: pointer to the first node where value is located.
- * resturns NULL if value is not present in the list or if head is NULL.
- **/
+ * linear_skip - Searches for an algorithm in a sorted singly
+ *               linked list of integers using linear skip.
+ * @list: A pointer to the  head of the linked list to search.
+ * @value: The value to search for.
+ *
+ * Return: If the value is not present or the head of the list is NULL, NULL.
+ *         Otherwise, a pointer to the first node where the value is located.
+ *
+ * Description: Prints a value every time it is compared in the list.
+ *              Uses the square root of the list size as the jump step.
+ */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-	if (!list)
-		return (list);
+	skiplist_t *node, *jump;
 
-	/* It's not what you expected, I still have to do this task */
-	value = value;
+	if (list == NULL)
+		return (NULL);
 
-	return (list);
+	for (node = jump = list; jump->next != NULL && jump->n < value;)
+	{
+		node = jump;
+		if (jump->express != NULL)
+		{
+			jump = jump->express;
+			printf("Value checked at index [%ld] = [%d]\n",
+					jump->index, jump->n);
+		}
+		else
+		{
+			while (jump->next != NULL)
+				jump = jump->next;
+		}
+	}
+
+	printf("Value found between indexes [%ld] and [%ld]\n",
+			node->index, jump->index);
+
+	for (; node->index < jump->index && node->n < value; node = node->next)
+		printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+	printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+
+	return (node->n == value ? node : NULL);
 }
